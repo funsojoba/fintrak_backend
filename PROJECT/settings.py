493f18ed6f8f0efb3f4a2443652340af6032392e
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2021-08-20 00:11:49 +0100

    swagger documentation

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     #INSTALLED APPS
+    'drf_yasg',
     'rest_framework',
     'rest_framework_simplejwt',
 ]
@@ -133,7 +134,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- PROJECT/urls.py
+++ PROJECT/urls.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from django.urls import path, include
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
+)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="FinTrak API",
+        default_version='v1',
+        description="Finanacial Analytics API",
+        contact=openapi.Contact(email="hrfunsojoba@gmail.local"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
@@ -10,4 +26,8 @@ urlpatterns = [
     path('auth/', include('authentication.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('swagger/', schema_view.with_ui('swagger',
+        cache_timeout=0), name='schema-swagger-ui'),
+
 ]
