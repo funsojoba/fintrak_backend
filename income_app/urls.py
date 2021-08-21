ebcfb24f0f503bd780e8c9e6522faf05add58705
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2021-08-21 15:56:29 +0100

    Income App completed

--- income_app/urls.py
+++ income_app/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-# from income_app.views.add_income import AddIncomeView
+from income_app.views.add_income import AddIncomeView
 from income_app.views.list_income import ListIncomeView
 from income_app.views.edit_income import EditIncomeView
+from income_app.views.delete_income import DeleteIncomeView
 
 urlpatterns = [
+    path('add', AddIncomeView.as_view(), name='add-income'),
     path('list', ListIncomeView.as_view(), name='list-income'),
     path('edit/<str:pk>', EditIncomeView.as_view(), name='edit-income'),
+    path('delete/<str:pk>', DeleteIncomeView.as_view(), name='delete-income'),
 ]
--- income_app/views/add_income.py
+++ income_app/views/add_income.py
@@ -1,10 +1,27 @@
 from rest_framework.views import APIView
 from rest_framework import permissions, status
-from rest_framework.response import Response
 
+from lib.response import Response
+from income_app.serializers.income_serializer import IncomeSerializer
+from income_app.models import Income
 
 class AddIncomeView(APIView):
     permission_classes = [permissions.IsAuthenticated]
-
+    serializer_class = IncomeSerializer
     def post(self, request):
-        pass
+        owner = request.user
+        data = request.data
+
+        serializer = self.serializer_class(data=data)
+
+        serializer.is_valid(raise_exception=True)
+
+        amount = data.get('amount', '')
+        source = data.get('source', '')
+        description = data.get('description', '')
+        income_date = data.get('income_date', '')
+
+        income = Income.objects.create(owner = owner, amount=amount, source=source, description=description, income_date=income_date)
+        income.save()
+
+        return Response(data={"message":"success"}, status=status.HTTP_201_CREATED)
