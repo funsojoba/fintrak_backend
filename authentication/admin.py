0faa04eee1c71c697f375070e809a770dbb8c7bb
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2021-09-04 17:10:01 -0500

    removed github action

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from django.contrib.auth.models import User
+from authentication.models.user import User
+# from django.contrib.auth.models import User
 
 
 admin.site.register(User)
