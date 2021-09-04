b0bf1cb10424560b74c97525f1992eab3e445381
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2021-09-04 17:52:26 -0500

    removed github action again

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
 # from django.contrib.auth.models import User
-from authentication.models.user import User
+from authentication.models import user
 from expense_app.models import Expense
 from income_app.models import Income
 from budget_app.models import BudgetExpense, BudgetIncome
 
 
-admin.site.register((User, Expense, Income, BudgetExpense, BudgetIncome))
+admin.site.register((user.User, Expense, Income, BudgetExpense, BudgetIncome))
