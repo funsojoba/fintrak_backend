2172166252c3e71f59843e26510edbceac594c6d
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2021-09-04 17:24:05 -0500

    updated authentication.user location

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+# from django.contrib.auth.models import User
+from authentication.models.user import User
+
+
+admin.site.register(User)
