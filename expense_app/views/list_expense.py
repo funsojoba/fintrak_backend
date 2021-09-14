aca8546683f57e99090999786f2e00569cbbba29
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2021-09-14 20:45:45 +0100

    updated list_expense view

--- expense_app/views/list_expense.py
+++ expense_app/views/list_expense.py
@@ -6,7 +6,7 @@ from rest_framework import views, status, permissions
 from lib.response import Response
 from expense_app.models import Expense
 from expense_app.serializers.expense_serializer import ExpenseSerializer
-
+from user_app.models import UserProfile
 
 class ListExpenseView(views.APIView):
     permission_classes = [permissions.IsAuthenticated]
@@ -18,7 +18,8 @@ class ListExpenseView(views.APIView):
         expense_query = Expense.objects.filter(owner=owner)
         serializer = self.serializer_class(expense_query, many=True)
         data = serializer.data
-        count = expense_query.count()
+
+        
 
         # expense per month
         current_month = datetime.now().month
@@ -35,11 +36,13 @@ class ListExpenseView(views.APIView):
         expense_by_category = Expense.objects.filter(owner=owner, expense_date__month=current_month).values(
             'category').annotate(category_total=Sum('amount'))
 
+        user_currency = UserProfile.objects.filter(user=request.user).first()
+        currency = user_currency.prefered_currency if user_currency else "$"
+
         data_response = {
-            "data": data,
-            "count": count,
             "expense_per_month": current_month_serializer.data,
             "total_expense": total_expense['amount__sum'] if total_expense['amount__sum'] else 0,
-            "expense_by_category": expense_by_category
+            "expense_by_category": expense_by_category,
+            "currency":currency
         }
         return Response(data=data_response, status=status.HTTP_200_OK)
