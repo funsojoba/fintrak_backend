943fdc3b5efa518f6179a73b5e8f737f1727dc12
Funso Joba <42432746+funsojoba@users.noreply.github.com>
2021-09-23 18:42:04 +0100

    rewrite admin.py dashboard

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
 # from django.contrib.auth.models import User
-from authentication.models import user
+from authentication.models.user import User
 from expense_app.models import Expense
 from income_app.models import Income
 from budget_app.models import BudgetExpense, BudgetIncome, TotalBudget
 
 
-admin.site.register((user.User, Expense, Income,
+admin.site.register((User, Expense, Income,
                     BudgetExpense, BudgetIncome, TotalBudget))
