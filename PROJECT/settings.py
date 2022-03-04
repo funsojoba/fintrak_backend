9e083cffad0fd1f9db682eac39ded846e8a186fe
Funso Joba <hrfunsojoba@gmail.com>
2022-03-04 23:56:25 +0100

    moved secret key to env file

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h#y9qfpv7c--gkx@8#@!pbi&6-koacim=8&n5e4*_nj-^qkedt'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -102,7 +102,6 @@ WSGI_APPLICATION = 'PROJECT.wsgi.application'
 
 DATABASE_URL = config(
     "DATABASE_URL",
-    default="postgres://pwowfnormuaeva:e5904f9f66d63b5e9bc1842a9875cd9dfaf58230027c9b0a86f28a2d2ab8275e@ec2-18-209-153-180.compute-1.amazonaws.com:5432/dedegosshiv0hq",
 )
 
 
