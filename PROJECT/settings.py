55a8ecbd2fa4d93dad11c08cdc04ec17a908f679
MAC <hrfunsojoba@gmail.com>
2023-04-12 19:25:53 +0100

    updated yaml file with env variables

--- PROJECT/settings.py
+++ PROJECT/settings.py
@@ -17,6 +17,10 @@ from pathlib import Path
 from decouple import config
 from datetime import timedelta
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
